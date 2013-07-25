eca9894f409bfaf5714ab817c3290958f5ad0054
Eugene Zakursky <Eugene.Zakursky@sigmaukraine.com>
2013-07-25 18:41:20 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ocstore/');
+define('HTTP_IMAGE', 'http://localhost/ocstore/image/');
+define('HTTP_ADMIN', 'http://localhost/ocstore/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ocstore/');
+define('HTTPS_IMAGE', 'http://localhost/ocstore/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\ocstore/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\ocstore/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\ocstore/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\ocstore/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\ocstore/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\ocstore/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\ocstore/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\ocstore/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\ocstore/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\ocstore/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fenrir');
+define('DB_PASSWORD', '1988');
+define('DB_DATABASE', 'ocstore');
+define('DB_PREFIX', '');
+?>
